89ee161257bd08b149ef8053fff9a74d47d07dfd
Kavil Jain <kav514.jain@gmail.com>
2022-05-17 10:54:31 +0530

    Added book api

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from crypt import methods
 from functools import wraps
-from urllib import response
+import bson
+from bson import ObjectId
 from flask import Flask, jsonify, render_template, send_from_directory, request
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
-from marshmallow import Schema, fields
+from marshmallow import Schema, ValidationError, fields, missing
 import jwt
 
 app = Flask(__name__, template_folder="swagger/templates")
@@ -51,22 +52,10 @@ def swagger_docs(path=None):
 def index():
     return """ <h1> Welcome to E-Library </h1> """
 
-# Authentication
 
-
-def tokenReq(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if "Authorization" in request.headers:
-            token = request.headers["Authorization"]
-            try:
-                jwt.decode(token, app.secret_key)
-            except:
-                return jsonify({"status": "fail", "message": "unauthorized"}), 401
-            return f(*args, **kwargs)
-        else:
-            return jsonify({"status": "fail", "message": "unauthorized"}), 401
-    return decorated
+# JWT AUTH
+jwt_scheme = {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
+spec.components.security_scheme("Authorization", jwt_scheme)
 
 # Schema
 
@@ -77,7 +66,7 @@ class SignupRequestSchema(Schema):
     password = fields.Str()
 
 
-class SignupResponseSchema(Schema):
+class MessageSchema(Schema):
     message = fields.Str()
 
 
@@ -86,13 +75,24 @@ class SigninRequestSchema(Schema):
     password = fields.Str()
 
 
-class SigninResponseSchema(Schema):
-    message = fields.Str()
+class BookSchema(Schema):
+    bookName = fields.Str()
+    author = fields.Str()
+
+
+class DeleteABookSchema(Schema):
+    book_id = fields.Str()
+
+
+class BookAvailabilitySchema(Schema):
+    book_id = fields.Str()
 
 # Routes
+# USERS
 
-@app.route('/signup', methods=['POST'])
-def signup():
+
+@app.route('/user/signup', methods=['POST'])
+def signupUser():
     """Signup user
         ---
         post:
@@ -103,23 +103,26 @@ def signup():
                     application/json:
                         schema: SignupRequestSchema
             responses: 
-                200:
+                201:
                     description: Return success message
                     content:     
                         application/json:
-                            schema: SignupResponseSchema
+                            schema: MessageSchema
                 401:
                     description: Duplicate email
                     content:
                         application/json:
-                            schema: SignupResponseSchema
+                            schema: MessageSchema
                 500:
                     description: Server Error
                     content:
                         application/json:
-                            schema: SignupResponseSchema             
+                            schema: MessageSchema             
 
     """
+    message = ""
+    code = 500
+    status = "fail"
     _json = request.json
     _name = _json['name']
     _email = _json['email']
@@ -130,31 +133,29 @@ def signup():
         check = mongo.db.users.find_one({"email": data['email']})
 
         if check:
-            resp = jsonify("user with that email exists")
-            resp.status_code = 401
-            return resp
+            message = "user with that email exists"
+            code = 401
+            status = "fail"
 
-        elif _name and _email and _password and request.method == 'POST':
+        else:
             _hased_password = generate_password_hash(_password)
             id = mongo.db.users.insert_one(
                 {'name': _name, 'email': _email, 'password': _hased_password})
-            resp = jsonify("User added successfully")
-            resp.status_code = 200
-            return resp
-
-        else:
-            return not_found()
+            if id.acknowledged:
+                status = "successful"
+                message = "user created successfully"
+                code = 201
 
     except Exception as ex:
-        message = {
-            'status': 500,
-            'message': 'Server Error' + request.url
-        }
-        return jsonify(message), 200
+        message = f"{ex}"
+        status = "fail"
+        code = 500
+
+    return jsonify({'status': status, "message": message}), code
 
 
-@app.route('/sigin', methods=['POST'])
-def sigin():
+@app.route('/user/signin', methods=['POST'])
+def signinUser():
     """Signin user
         ---
         post:
@@ -163,25 +164,29 @@ def sigin():
                 required: true
                 content:
                     application/json:
-                        schema: SigninRequestSchema
+                        schema: SigninRequestSchema        
             responses: 
                 200:
                     description: Return success message
                     content:     
                         application/json:
-                            schema: SigninResponseSchema
+                            schema: MessageSchema
                 401:
                     description: Invalid Credentials
                     content:
                         application/json:
-                            schema: SigninResponseSchema
+                            schema: MessageSchema
                 500:
                     description: Server Error
                     content:
                         application/json:
-                            schema: SigninResponseSchema             
+                            schema: MessageSchema             
 
     """
+    message = ""
+    res_data = {}
+    code = 500
+    status = "fail"
     try:
         data = request.get_json()
         user = mongo.db.users.find_one({"email": data["email"]})
@@ -198,33 +203,370 @@ def sigin():
 
                 del user['password']
 
-                resp = jsonify("User authenticated")
-                resp.status_code = 200
-                resp.status = "success"
-                resp_data = {"token": token}
+                message = f"user authenticated"
+                code = 200
+                status = "successful"
+                res_data['token'] = token
+                res_data['user'] = user
 
             else:
-                resp = jsonify("wrong password")
-                resp.status_code = 401
-                resp.status = "fail"
-                resp_data = None
+                message = "wrong password"
+                code = 401
+                status = "fail"
         else:
-            resp = jsonify("invalid login details")
-            resp.status_code = 401
-            resp.status = "fail"
-            resp_data = None
+            message = "invalid login details"
+            code = 401
+            status = "fail"
 
     except Exception as ex:
-        resp = jsonify(ex)
-        resp.status_code = 500
-        resp.status = "fail"
-        resp_data = None
-    return resp, resp_data
+        message = f"{ex}"
+        code = 500
+        status = "fail"
+    return jsonify({'status': status, "data": res_data, "message": message}), code
+
+
+@app.route('/user/update', methods=['PUT'])
+def updateUser():
+    """Update User Details
+        ---
+        put:
+            description: Update a user
+            security:
+                - Authorization: []
+            requestBody:
+                required: true
+                content:
+                    application/json:
+                        schema: SignupRequestSchema
+            responses: 
+                201:
+                    description: Return success message
+                    content:     
+                        application/json:
+                            schema: MessageSchema
+                404:
+                    description: Failed
+                    content:
+                        application/json:
+                            schema: MessageSchema
+                500:
+                    description: Server Error
+                    content:
+                        application/json:
+                            schema: MessageSchema             
+
+    """
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['password']
+    data = {}
+    loggedInUser = {}
+    code = 500
+    message = ""
+    status = "fail"
+    if "Authorization" in request.headers:
+        token = request.headers["Authorization"][7:]
+        try:
+            loggedInUser = jwt.decode(
+                token, app.secret_key, algorithms=["HS256"])
+        except:
+            return jsonify({"status": "fail", "message": "unauthorized"}), 401
+    else:
+        return jsonify({"status": "fail", "message": "unauthorized"}), 401
+
+    try:
+        if (request.method == 'PUT'):
+            user_id = loggedInUser['user']['id']
+            filter = {"_id": ObjectId(user_id)}
+            _hased_password = generate_password_hash(_password)
+            data = {"name": _name, "email": _email,
+                    "password": _hased_password}
+            updatedData = {"$set": data}
+            check = mongo.db.users.find_one({"email": _email})
+            if not check:
+                res = mongo.db.users.update_one(filter, updatedData)
+                if res:
+                    message = "updated successfully"
+                    status = "successful"
+                    code = 201
+                else:
+                    message = "update failed"
+                    status = "fail"
+                    code = 404
+            else:
+                return jsonify({"status": "fail", "message": "duplicateEmail"}), 401
+        else:
+            not_found()
+
+    except Exception as ee:
+        message = str(ee)
+        status = "Error"
+
+    return jsonify({"status": status, "message": message, 'data': data}), code
+
+
+@app.route('/user/delete', methods=['DELETE'])
+def deleteUser():
+    """Delete User 
+        ---
+        delete:
+            description: Remove User
+            security:
+                - Authorization: []
+            responses: 
+                201:
+                    description: Return success message
+                    content:     
+                        application/json:
+                            schema: MessageSchema
+                404:
+                    description: Failed
+                    content:
+                        application/json:
+                            schema: MessageSchema
+                500:
+                    description: Server Error
+                    content:
+                        application/json:
+                            schema: MessageSchema             
+
+    """
+    loggedInUser = {}
+    code = 500
+    message = ""
+    status = "fail"
+    if "Authorization" in request.headers:
+        token = request.headers["Authorization"][7:]
+        try:
+            loggedInUser = jwt.decode(
+                token, app.secret_key, algorithms=["HS256"])
+        except:
+            return jsonify({"status": "fail", "message": "unauthorized"}), 401
+    else:
+        return jsonify({"status": "fail", "message": "unauthorized"}), 401
+
+    try:
+        if (request.method == 'DELETE'):
+            user_id = loggedInUser['user']['id']
+            res = mongo.db.users.delete_one({"_id": ObjectId(user_id)})
+            if res:
+                message = "deleted successfully"
+                status = "successful"
+                code = 201
+            else:
+                message = "delete failed"
+                status = "fail"
+                code = 404
+        else:
+            not_found()
+
+    except Exception as ee:
+        message = str(ee)
+        status = "Error"
+
+    return jsonify({"status": status, "message": message}), code
+
+# BOOKS
+
+
+@app.route('/add-book', methods=['POST'])
+def addBook():
+    """Add a Book
+        ---
+        post:
+            description: Add a Book
+            security:
+                - Authorization: []
+            requestBody:
+                required: true
+                content:
+                    application/json:
+                        schema: BookSchema
+            responses: 
+                201:
+                    description: Return success message
+                    content:     
+                        application/json:
+                            schema: MessageSchema
+                401:
+                    description: Adding a Book Failed
+                    content:
+                        application/json:
+                            schema: MessageSchema
+                500:
+                    description: Server Error
+                    content:
+                        application/json:
+                            schema: MessageSchema             
+
+    """
+    _json = request.json
+    _bookName = _json['bookName']
+    _author = _json['author']
+    code = 500
+    message = ""
+    status = "fail"
+    if "Authorization" in request.headers:
+        token = request.headers["Authorization"][7:]
+        try:
+            loggedInUser = jwt.decode(
+                token, app.secret_key, algorithms=["HS256"])
+        except:
+            return jsonify({"status": "fail", "message": "unauthorized"}), 401
+    else:
+        return jsonify({"status": "fail", "message": "unauthorized"}), 401
+
+    try:
+        if (request.method == 'POST'):
+            user_id = loggedInUser['user']['id']
+            print(user_id)
+            res = mongo.db.books.insert_one(
+                {"bookName": _bookName, "author": _author, "user_id": user_id, "availability": "available"})
+            if res.acknowledged:
+                message = "Book added successfully"
+                status = "successful"
+                code = 201
+            else:
+                message = "Adding book failed"
+                status = "fail"
+                code = 404
+        else:
+            not_found()
+
+    except Exception as ee:
+        message = str(ee)
+        status = "Error"
+
+    return jsonify({"status": status, "message": message}), code
+
+
+@app.route('/delete-book/<book_id>', methods=['DELETE'])
+def removeBook(book_id):
+    """Remove a Book
+        ---
+        delete:
+            description: Remove a Book
+            security:
+                - Authorization: []
+            requestBody:
+                required: true
+                content:
+                    application/json:
+                        schema: DeleteABookSchema
+            responses: 
+                201:
+                    description: Return success message
+                    content:     
+                        application/json:
+                            schema: MessageSchema
+                404:
+                    description: Adding a Book Failed
+                    content:
+                        application/json:
+                            schema: MessageSchema
+                500:
+                    description: Server Error
+                    content:
+                        application/json:
+                            schema: MessageSchema             
+
+    """
+    data = {}
+    code = 500
+    message = ""
+    status = "fail"
+    try:
+        if (request.method == 'DELETE'):
+            print(ObjectId(book_id))
+            # data = mongo.db.books.find_one({"_id": ObjectId(book_id)})
+            res = mongo.db.books.delete_one({"_id": ObjectId(book_id)})
+            if res:
+                message = "Delete successfully"
+                status = "successful"
+                code = 201
+            else:
+                message = "Delete failed"
+                status = "fail"
+                code = 404
+        else:
+            message = "Delete Method failed"
+            status = "fail"
+            code = 404
+
+    except Exception as ee:
+        message = str(ee)
+        status = "Error"
+
+    return jsonify({"status": status, "message": message, 'data': data}), code
+
+
+@app.route('/temporary-remove-book/<book_id>', methods=['PUT'])
+def removeBookTemp(book_id):
+    """Remove a Book Temporarily
+        ---
+        put:
+            description: Make book Unavailable
+            security:
+                - Authorization: []
+            requestBody:
+                required: true
+                content:
+                    application/json:
+                        schema: BookAvailabilitySchema
+            responses: 
+                201:
+                    description: Return success message
+                    content:     
+                        application/json:
+                            schema: MessageSchema
+                404:
+                    description: Adding a Book Failed
+                    content:
+                        application/json:
+                            schema: MessageSchema
+                500:
+                    description: Server Error
+                    content:
+                        application/json:
+                            schema: MessageSchema             
+    """
+    code = 500
+    message = ""
+    status = "fail"
+    try:
+        if (request.method == 'PUT'):
+            filter = {"_id": ObjectId(book_id)}
+            markUnavialable = {"$set": {"availability": "not available"}}
+            res = mongo.db.books.update_one(filter, markUnavialable)
+            if res:
+                message = "Book is made temporarily unavialble"
+                status = "successful"
+                code = 201
+            else:
+                message = "Marking unavailable failed"
+                status = "fail"
+                code = 404
+        else:
+            message = "Updation Method failed"
+            status = "fail"
+            code = 404
+
+    except Exception as ee:
+        message = str(ee)
+        status = "Error"
+
+    return jsonify({"status": status, "message": message}), code
 
 
 with app.test_request_context():
-    spec.path(view=signup)
-    spec.path(view=sigin)
+    spec.path(view=signupUser)
+    spec.path(view=signinUser)
+    spec.path(view=updateUser)
+    spec.path(view=deleteUser)
+    spec.path(view=addBook)
+    spec.path(view=removeBook)
+    spec.path(view=removeBookTemp)
 
 
 @app.errorhandler(404)
